205e09fe46e8fbd4a2b40144e5ed2b28c8040a3e
Asmund Berge <herrasbe@gmail.com>
2016-01-23 10:55:14 +0100

    Add passport.seializeUser and deserializeUser

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,14 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user');
 
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
