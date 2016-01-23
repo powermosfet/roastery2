f29709dedb4c1cc0c1958c2fc2db334ed00860a5
Asmund Berge <herrasbe@gmail.com>
2016-01-23 12:33:13 +0100

    Typo in last commit

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user');
-var config = require('../config/');
+var config = require('../config');
 
 passport.serializeUser(function(user, done) {
     done(null, user);
