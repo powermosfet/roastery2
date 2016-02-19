3f0cc6fe1e75e46cef2749b9ae8bb445e334197e
Asmund Berge <herrasbe@gmail.com>
2016-02-19 15:54:25 +0100

    insert AppBar into main page

--- frontend/js/app.jsx
+++ frontend/js/app.jsx
@@ -1,8 +1,19 @@
 var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var injectTapEventPlugin = require('react-tap-event-plugin');
+var AppBar = require('material-ui/lib/app-bar');
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />,
   $('.content')[0]
 );
