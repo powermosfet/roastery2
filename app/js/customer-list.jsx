b4f12621035c0580f7f1b4d0ada39ea331df4a86
Asmund Berge <herrasbe@gmail.com>
2016-02-24 22:29:12 +0100

    Fetch cusotmers from api

--- app/js/customer-list.jsx
+++ app/js/customer-list.jsx
@@ -4,6 +4,26 @@ var ListItem = require('material-ui/lib/lists/list-item');
 var Avatar = require('material-ui/lib/svg-icons/action/info');
 
 var CustomerList = React.createClass({
+  loadCustomers: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCustomers();
+    // setInterval(this.loadCustomers, this.props.pollInterval);
+  },
   render: function() {
     var customerItems = this.props.data.map(function(customer) {
       var firstLetter = customer.name[0] || 'X';
