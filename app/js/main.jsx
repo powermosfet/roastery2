cd84d5844a445e791c70881aca5a412aa26d3e0d
Asmund Berge <herrasbe@gmail.com>
2016-02-25 06:48:47 +0100

    add FAB button

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var AppBar = require('material-ui/lib/app-bar');
 var CustomerList = require('./customer-list');
+var ContentAdd = require('material-ui/lib/svg-icons/content/add');
 
 module.exports = React.createClass({
   render: function() {
@@ -11,6 +12,7 @@ module.exports = React.createClass({
       iconClassNameRight="muidocs-icon-navigation-expand-more"
     />
     <CustomerList url="/api/customers" />
+    <ContentAdd />
   </div>
     );
   },
