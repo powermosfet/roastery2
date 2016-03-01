6f0e2dfd4c417f8a790f4ea8b0207488ef7f00a4
Asmund Berge <herrasbe@gmail.com>
2016-03-01 13:55:34 +0100

    Add leftNav

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,21 +1,30 @@
 var React = require('react');
 var AppBar = require('material-ui/lib/app-bar');
+var LeftNav = require('material-ui/lib/left-nav');
 var CustomerList = require('./customer-list');
 var ContentAdd = require('material-ui/lib/svg-icons/content/add');
 var IconButton = require('material-ui/lib/icon-button');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return { leftNavOpen: false };
+  },
+  handleMenuClick: function() {
+    this.state.leftNavOpen = !this.state.leftNavOpen;
+  },
   render: function() {
     return (
   <div>
     <AppBar
       title="Customers"
+      onLeftIconButtonTouchTap={handleMenuClick}
       iconElementRight={
         <IconButton>
           <ContentAdd />
         </IconButton>
       }
     />
+    <LeftNav open={this.state.leftNavOpen} />
     <CustomerList url="/api/customers" />
   </div>
     );
