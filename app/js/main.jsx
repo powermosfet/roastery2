258ad3258b45f474a0f2a70dbb0a92e5a6f0f463
Asmund Berge <herrasbe@gmail.com>
2016-03-28 11:46:03 +0200

    Sass it up a bit

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
 var CustomerList = require('./modules/customer-list');
 
+require('../css/main.scss');
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className="page">
         <CustomerList url="/api/customers" />
       </div>
     );
